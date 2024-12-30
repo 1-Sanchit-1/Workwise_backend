b148784e7231f177701dee694516607b66865978
1-Sanchit-1 <sanchitgupta9450@gmail.com>
2024-12-30 10:52:04 +0530

    updates

--- index.js
+++ index.js
@@ -3,42 +3,20 @@ const sequelize = require("./config/db");
 const dotenv = require("dotenv");
 const Routes = require("../Backend/routers/routes");
 const cors = require("cors");
+
 dotenv.config();
 
 const app = express();
-
-const corsOptions = {
-  origin: "*",
-  methods: ["GET", "POST", "PUT", "DELETE"],
-  allowedHeaders: ["Content-Type", "Authorization"],
-};
-
-app.use(cors(corsOptions));
-
+app.use(cors());
 app.use(express.json());
 
-// Test database connection
 sequelize
   .authenticate()
   .then(() => {
     console.log("Database connected...");
-    (async () => {
-      try {
-        await sequelize.sync({ alter: false });
-        console.log("Database synchronized successfully!");
-      } catch (error) {
-        console.error("Error synchronizing the database:", error);
-      }
-    })();
+    sequelize.sync({ alter: false });
   })
-  .catch((err) => {
-    console.error("Database connection failed:", err);
-  });
-
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
-});
+  .catch((err) => console.error("Database connection failed:", err));
 
 app.use("/api", Routes);
 
